6b6c93c7e8854edbcf5e2bc7d5c86fed9041d659
charles-na <nyk0806@naver.com>
2019-02-08 09:46:08 +0900

    2.21 Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:232323,
+        title: "Video awesome",
+        description: "This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Charles",
+            email: "charles@gmail.com"
+        }
+    },
+    {
+        id:454545,
+        title: "Video super",
+        description: "This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Charles",
+            email: "charles@gmail.com"
+        }
+    },
+    {
+        id:555555,
+        title: "Video nice",
+        description: "This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Charles",
+            email: "charles@gmail.com"
+        }
+    },
+    {
+        id:11111,
+        title: "Video perfect",
+        description: "This is something I love",
+        views:24,
+        videoFile:"https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Charles",
+            email: "charles@gmail.com"
+        }
+    }
+]
